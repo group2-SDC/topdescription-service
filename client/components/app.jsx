f2aaa3b86e50893e182602bfb95a56983043e8f6
Chris Hancock <christopher.hancock0331@gmail.com>
2020-11-11 15:11:13 -0800

    Init setup, dependencies, service framework

---
+++ client/components/app.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            initial: ''
+        };
+    }
+
+    render() {
+        return(
+            <div>React Is Working</div>
+        )
+    }
+}
+
+export default App;
