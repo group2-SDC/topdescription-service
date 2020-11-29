5fb309e3092da29a5c55124f9412bcb895ddae71
Chris Hancock <christopher.hancock0331@gmail.com>
2020-11-28 21:36:50 -0800

    added jest/enzyme config files

---
+++ jest.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+
+    // Automatically clear mock calls and instances between every test
+    clearMocks: true,
+  
+    // The directory where Jest should output its coverage files
+    coverageDirectory: "coverage",
+  
+    // An array of regexp pattern strings used to skip coverage collection
+    coveragePathIgnorePatterns: [
+      "/node_modules/"
+    ],
+  
+    // Indicates which provider should be used to instrument code for coverage
+    coverageProvider: "v8",
+  
+    // An array of file extensions your modules use
+    moduleFileExtensions: [
+      "js",
+      "jsx"
+    ],
+  
+    // A list of paths to modules that run some code to configure or set up the testing framework before each test
+    setupFilesAfterEnv: ['<rootDir>/enzyme.config.js'],
+  
+    // The test environment that will be used for testing
+    testEnvironment: "node",
+  
+    // The glob patterns Jest uses to detect test files
+    testMatch: [
+      "**/__tests__/**/*.[jt]s?(x)",
+      "**/?(*.)+(spec|test).[tj]s?(x)"
+    ]
+  };
