c7e549a6eba44ef8d84bc15f5a5baa4d9448da8f
Chris Hancock <christopher.hancock0331@gmail.com>
2020-12-01 13:15:40 -0800

    Card component height change

--- client/components/StyledComponents/CardComponentStyle.js
+++ client/components/StyledComponents/CardComponentStyle.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
-const CardComponent = styled.section`
+const CardComponent = window.styled.section`
     display: flex;
     box-shadow: 1px 1px 2px grey;
     padding-left: 10px;
     width: 315px;
-    height: 525px;
+    height: 500px;
 `;
 
 export default CardComponent;
