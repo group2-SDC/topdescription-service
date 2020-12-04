267d8c334e31b2a7baed5a2ffecd886644012c1a
Annie Heo <annie.heo@gmail.com>
2020-12-04 11:17:51 -0800

    downloaded 1000 images for gallery  and gitignored it

---
+++ database/postGres/index.js
@@ -0,0 +1,11 @@
+// const { Pool } = require('pg');
+
+// //use pool for multiple concurrent requests
+// const pool = new Pool();
+
+// pool.query('SELECT * FROM LISTINGS', (err, res) => {
+//   console.log(err, res);
+//   pool.end();
+// });
+
+//not used for now
